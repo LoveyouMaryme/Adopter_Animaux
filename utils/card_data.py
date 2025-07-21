4289ddcf20a69c39534cf6a8a1bc69ef15a2512d
LoveyouMaryme <love-mary.victor@outlook.com>
2025-07-21 19:20:34 -0400

    forgot some added files

---
+++ utils/card_data.py
@@ -0,0 +1,23 @@
+card_index_dict = card_index = [
+    {
+        "image_link" : "images/icons/finding-pet.png",
+        "text" :  """Parmi tous ces regards, 
+        il y a peut-être celui qui changera votre vie. 
+        Parcourez nos compagnons à la recherche d’un foyer 
+        et trouvez votre futur ami.""",
+        "text_button" : "Trouvez votre ami"
+    },
+    {
+        "image_link" : "images/icons/house-pet.png",
+        "text" :  """Votre compagnon mérite un foyer aimant.
+             Mettez-le en adoption dès aujourd’hui
+            et permettez à une famille de le rencontrer.""",
+        "text_button" : "Trouvez une maison"
+    },
+    {
+        "image_link" : "images/icons/team.png",
+        "text" :  """Rencontrez les humains au grand cœur qui veillent sur chaque compagnon.
+        Découvrez notre mission, nos valeurs, et les visages de ceux qui rendent tout cela possible.""",
+        "text_button" : "Rencontrez-nous"
+    }
+]
---
+++ utils/helpers.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import render_template
+from flask import g
+from ..database import Database
+from ..utils.card_data import card_index_dict
+import math
+import random
+
+
+
+def get_animals_carousel(db):
+    
+    """
+    Description : Select the animals that will start the  in the index page
+
+    """
+    
+    list_animaux = db.get_animaux()
+    num_animaux = len(list_animaux)
+    num_carousel = random.randrange(num_animaux - 1)
+    num_picked = 0
+    picked_animals = []
+
+    
+    while num_picked < 4:
+        animal = list_animaux[num_carousel]
+        picked_animals.append(animal)
+  
+
+        num_carousel += 1
+        num_picked += 1
+
+        if (num_carousel >= len(list_animaux)):
+            num_carousel = 0
+            
+        
+    return picked_animals
+        
+        
+
+
+
