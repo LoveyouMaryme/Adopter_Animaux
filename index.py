15977efcfcda8dc23dab4701e4d5ef72e7f9b9d6
LoveyouMaryme <love-mary.victor@outlook.com>
2025-07-21 12:09:08 -0400

    adding jinja for index-cards

--- index.py
+++ index.py
@@ -37,5 +37,30 @@ def close_connection(exception):
 
 @app.route('/')
 def index():
-    # À remplacer par le contenu de votre choix.
-    return render_template('index.html')
+
+    card_index_dict = card_index = [
+        {
+        "image_link" : "images/icons/finding-pet.png",
+        "text" :  """Parmi tous ces regards, 
+        il y a peut-être celui qui changera votre vie. 
+        Parcourez nos compagnons à la recherche d’un foyer 
+        et trouvez votre futur ami.""",
+        "text_button" : "Trouvez votre ami"
+    },
+    {
+        "image_link" : "images/icons/house-pet.png",
+        "text" :  """Votre compagnon mérite un foyer aimant.
+             Mettez-le en adoption dès aujourd’hui
+            et permettez à une famille de le rencontrer.""",
+        "text_button" : "Trouvez une maison"
+    },
+    {
+        "image_link" : "images/icons/team.png",
+        "text" :  """Rencontrez les humains au grand cœur qui veillent sur chaque compagnon.
+Découvrez notre mission, nos valeurs, et les visages de ceux qui rendent tout cela possible.""",
+        "text_button" : "Rencontrez-nous"
+    }
+    ]
+    animaux_db = get_db()
+    print(animaux_db.get_animaux())
+    return render_template('index.html', card_index=card_index_dict)
