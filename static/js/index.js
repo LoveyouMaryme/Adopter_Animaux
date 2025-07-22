c3ecf926c689e2cb77d0d53f1222a2cbf9e22a9a
LoveyouMaryme <love-mary.victor@outlook.com>
2025-07-21 21:37:06 -0400

    forgot my index.js

---
+++ static/js/index.js
@@ -0,0 +1,9 @@
+changePetElement = document.getElementsByClassName("pet-switch");
+getPetCards = document.getElementsByClassName("pet-card");
+
+parentContainerPetCards = leftChangeElement = changePetElement[0];
+rightChangeElement = changePetElement[1];
+
+console.log(leftChangeElement);
+
+leftChangeElement.addEventListener("click", () => {});
