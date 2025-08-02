a97995fae10247b91abbb745c462fa145633d21e
LoveyouMaryme <love-mary.victor@outlook.com>
2025-08-02 13:34:25 -0400

    mettre les variables en snakecase

--- index.py
+++ index.py
@@ -26,16 +26,11 @@ from .utils import helpers
 import re
 from flask import jsonify
 
-# Thank you : https://flask.palletsprojects.com/en/stable/patterns/fileuploads/
-
 UPLOAD_FOLDER = 'static\images\photo_animaux'
-
 
 app = Flask(__name__, static_url_path="", static_folder="static")
 app.config['SECRET_KEY'] = 'ourbigsecret'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -43,13 +38,11 @@ def get_db():
         g._database = Database()
     return g._database
 
-
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.disconnect()
-
 
 @app.route('/')
 def index():
@@ -59,7 +52,6 @@ def index():
     session['random_list_animaux'] = random_list_animaux
     session['carousel'] = carousel
     return render_template('index.html', cards=card_index_dict, carousel=carousel)
-
 
 @app.route('/api/get_animal_carousel')
 def get_next_animal_carousel():
@@ -75,13 +67,11 @@ def get_next_animal_carousel():
     session['carousel'] = carousel
     carousel_ids = [animal["id"] for animal in carousel]
 
-
     available = [
         animal["id"]
         for animal in random_list_animaux
         if animal["id"] not in recently_shown_ids and animal["id"] not in carousel_ids
     ]
-    
     
     if available:
         next_animal = animaux_db.get_animal(available[0])
@@ -93,15 +83,11 @@ def get_next_animal_carousel():
     
     session['carousel'] = carousel
     session['recently_shown'] = recently_shown_ids
-    
 
     return carousel
 
-
 @app.route("/adoption")
 def page_adoption():
-
-
     animaux_db = get_db()
     animaux_une_race = animaux_db.get_espece("chat")
     nb_animaux_by_race = len(animaux_une_race)
@@ -110,23 +96,18 @@ def page_adoption():
 
     return render_template("page_adoption.html", pets=animaux_une_race, nbr=nb_animaux_by_race, race='chat')
 
-
-
 @app.route("/adoption/<animal_type>")
 def page_adoption_by_race(animal_type):
-
     animaux_db = get_db()
     animaux_une_race = animaux_db.get_espece(animal_type)
     print(animaux_une_race)
     nb_animaux_by_race = len(animaux_une_race)
     print(nb_animaux_by_race)
 
-
     return render_template(f"adoption_{animal_type}.html", pets=animaux_une_race, nbr=nb_animaux_by_race, race=animal_type)
 
 @app.route("/adoption/autre")
 def page_adoption_autre():
-
     animaux_db = get_db()
     animaux_une_race = animaux_db.get_uncommon()
     print(animaux_une_race)
@@ -135,12 +116,9 @@ def page_adoption_autre():
     
     return render_template(f"adoption_autre.html", pets=animaux_une_race, nbr=nb_animaux_by_race, race="autre")
 
-
-    
 @app.route("/reloger_un_animal")
 def page_reloger():
     return render_template("formulaire_adoption.html")
-
 
 @app.route("/animal_descr_page/<pet_id>")
 def page_descr_animal(pet_id):
@@ -148,7 +126,7 @@ def page_descr_animal(pet_id):
     pet = animaux_db.get_animal(pet_id)
     print("testing pet")
     print(pet)
-    return render_template(f"animal_descr_page.html", fiche_animal = pet)
+    return render_template(f"animal_descr_page.html", fiche_animal=pet)
 
 @app.route("/contactez_nous")
 def page_contact():
@@ -162,7 +140,6 @@ def page_recherche_avance():
     print(list_espece)
     return render_template("recherche_avance.html", especes=list_espece)
 
-
 @app.route("/api/races", methods=["GET"])
 def get_races_per_espece():
     especes = request.args.getlist('especes')
@@ -171,7 +148,6 @@ def get_races_per_espece():
     five_common_race = animaux_db.get_five_most_common_race(especes)
     return jsonify(five_common_race)
 
-    
 @app.route("/api/results", methods=['GET'])
 def get_results():
     especes = request.args.getlist('especes')
@@ -179,7 +155,7 @@ def get_results():
     results = []
     animaux_db = get_db()
 
-    if(especes or races):
+    if especes or races:
         results = animaux_db.get_result_research(especes, races)
     print(results)
     return jsonify(results)
@@ -190,35 +166,29 @@ def get_results_from_searchbar():
     filters = request.args.getlist('filters')
     print(filters)
 
-
     results = animaux_db.get_data_everywhere(filters)
     return jsonify(results)
 
-
-
-
 @app.route('/register_animal', methods=['POST'])
 def register_animal():
-
-    email_rx   = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
-    cp_rx      = r'^[A-Z]\d[A-Z]\s?\d[A-Z]\d$'
+    email_rx = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'
+    cp_rx = r'^[A-Z]\d[A-Z]\s?\d[A-Z]\d$'
     address_rx = r'^[0-9]+\s+[A-Za-zÀ-Ö\' -]+$'
-    city_rx    = r'^[A-Za-zÀ-Ö\' -]+$'
+    city_rx = r'^[A-Za-zÀ-Ö\' -]+$'
 
     try:
-        animal_db = get_db()
-        petName = request.form.get('name')
-        petAge = int(request.form.get('age'))
-        petRace = request.form.get('race')
-        petEspece = request.form.get('espece')
-        petDescription = request.form.get('description')
-        ownerEmail = request.form.get('email')
-        ownerAddress = request.form.get('address')
-        ownerCity = request.form.get('city')
-        ownerCp = request.form.get('cp')
-    
+        animaux_db = get_db()
+        pet_name = request.form.get('name')
+        pet_age = int(request.form.get('age'))
+        pet_race = request.form.get('race')
+        pet_espece = request.form.get('espece')
+        pet_description = request.form.get('description')
+        owner_email = request.form.get('email')
+        owner_address = request.form.get('address')
+        owner_city = request.form.get('city')
+        owner_cp = request.form.get('cp')
 
-        information_list = [petName, petAge, petRace, petEspece, petDescription, ownerEmail, ownerAddress, ownerCity, ownerCp]
+        information_list = [pet_name, pet_age, pet_race, pet_espece, pet_description, owner_email, owner_address, owner_city, owner_cp]
 
         # Did I get all my pet data ?
         if any(information is None for information in information_list):
@@ -226,35 +196,24 @@ def register_animal():
         
         # Verification back-end
         valid = (
-            petAge >= 0 and petAge <=20 and
-            len(petName) >= 3 and len(petName) <= 20 and
-            re.fullmatch(email_rx, ownerEmail) and
-            re.fullmatch(cp_rx, ownerCp) and
-            re.fullmatch(address_rx, ownerAddress) and
-            re.fullmatch(city_rx, ownerCity)
-
+            pet_age >= 0 and pet_age <= 20 and
+            len(pet_name) >= 3 and len(pet_name) <= 20 and
+            re.fullmatch(email_rx, owner_email) and
+            re.fullmatch(cp_rx, owner_cp) and
+            re.fullmatch(address_rx, owner_address) and
+            re.fullmatch(city_rx, owner_city)
         )
 
         if not valid:
             return redirect(url_for('error_page.html'))
         
-    
         print("is it after")
-        animal_db.add_animal(petName, petEspece, petRace, petAge, petDescription, ownerEmail, ownerAddress, ownerCity, ownerCp)
+        animaux_db.add_animal(pet_name, pet_espece, pet_race, pet_age, pet_description, owner_email, owner_address, owner_city, owner_cp)
         print("is it before")
 
-        lastAnimalId = animal_db.get_last_animal()['id']
-        return redirect(url_for('page_descr_animal', pet_id=lastAnimalId))
+        last_animal_id = animaux_db.get_last_animal()['id']
+        return redirect(url_for('page_descr_animal', pet_id=last_animal_id))
     
     except Exception as e:
         print(f"there's an error {e}")
         return redirect(url_for('error_page.html'))
-
-            
-
-
-
-    
-
-
-    
